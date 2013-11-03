c60b713369dc9ad7d75464c50d52fa9f6d9cad4e
Scott <Scottage@scotts-air.home>
2013-11-03 00:32:44 +0000

    Rails app set-up
    Full rails app set-up

---
+++ urlapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Urlapp::Application.config.secret_key_base = '6eadbb6594f6d1baaebe2895f878970a20abb12a56739bc77b670bfa5a2ff6e6a627b96ca0b1ef9f3a3b8aa26319859ac270b8e9e94d94251b268cf5f204a204'
